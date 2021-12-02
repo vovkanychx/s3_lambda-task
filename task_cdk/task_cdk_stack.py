e2c569e1680c36aa91719f4b0245fec7d6ea2b48
Vovkanych Dmytro <dima.vovkanych24@gmail.com>
2021-12-02 17:43:19 +0200

    Update

--- task_cdk/task_cdk_stack.py
+++ task_cdk/task_cdk_stack.py
@@ -1,24 +1,53 @@
 from aws_cdk import (
-    core as cdk
-    # aws_sqs as sqs,
+     core as cdk,
+     aws_s3 as s3,
+     aws_lambda as lambda_,
+     aws_s3_notifications as s3_notify,
 )
-
-# For consistency with other languages, `cdk` is the preferred import name for
-# the CDK's core module.  The following line also imports it as `core` for use
-# with examples from the CDK Developer's Guide, which are in the process of
-# being updated to use `cdk`.  You may delete this import if you don't need it.
-from aws_cdk import core
-
+import os, boto3, logging
+from botocore.client import ClientError
 
 class TaskCdkStack(cdk.Stack):
 
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        #create new s3 bucket and src&dest folders in it
+        s3_client = boto3.client('s3')
+        s3_resource = boto3.resource('s3')
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "TaskCdkQueue",
-        #     visibility_timeout=cdk.Duration.seconds(300),
-        # )
+        def create_bucket(bucket_name, region=None):
+            try:
+                if region is None:
+                    s3_client = boto3.client('s3')
+                    s3_client.create_bucket(Bucket=bucket_name)
+                else:
+                    s3_client = boto3.client('s3', region_name=region)
+                    location = {'LocationConstraint': region}
+                    s3_client.create_bucket(Bucket=bucket_name,
+                                            CreateBucketConfiguration=location)
+            except ClientError as e:
+                logging.error(e)
+                return False
+            return True
+
+        bucket_name = 'task-buck'
+        region = 'eu-central-1'
+        create_bucket(bucket_name, region)
+        
+        src_dir_name = "task-directory-source"
+        dest_dir_name = "task-directory-destination"
+        s3_client.put_object(Bucket = bucket_name, Key = (src_dir_name+'/'))
+        s3_client.put_object(Bucket = bucket_name, Key = (dest_dir_name+'/'))
+
+
+#create lambda function
+        function = lambda_.Function(self, "TaskLambdaFunction",
+                                    runtime = lambda_.Runtime.PYTHON_3_9,
+                                    handler = "lambda_handler.main",
+                                    code = lambda_.Code.from_asset("./lambda")
+        )
+        
+#send notification to lambda & create notification when object put in bucket
+        notification = s3_notify.LambdaDestination(function)
+        #bucket.add_event_notification(s3.EventType.OBJECT_CREATED_PUT, notification)
